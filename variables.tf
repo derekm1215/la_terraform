a782c0980226813a0a4a2dbb966aa944e4edd153
ithotsauce <derekm1215@gmail.com>
2016-11-15 13:02:32 -0500

    lots of added vars

--- variables.tf
+++ variables.tf
@@ -1,18 +1,38 @@
-variable "localip" {
-    default = "76.24.28.162/32"
-}
+variable "localip" {}
 
-variable "aws_profile" {
-    default = "linuxacademy"
-}
+variable "aws_profile" {}
 
-variable "dbuser" {
-    default = "dmorgan"
-}
+variable "dbname" {}
 
-variable "dbpass" {
-    default = "dmorgantest"
-}
+variable "dbuser" {}
+
+variable "dbpass" {}
+
+variable "db_instance_class" {}
+
+variable "dev_instance_type" {}
+
+variable "lc_instance_type" {}
+
+variable "dev_ami" {}
+
+variable "elb_healthy_threshold" {}
+
+variable "elb_unhealthy_threshold" {}
+
+variable "elb_timeout" {}
+
+variable "elb_interval" {}
+
+variable "asg_max" {}
+
+variable "asg_min" {}
+
+variable "asg_grace" {}
+
+variable "asg_hct" {}
+
+variable "asg_cap" {}
 
 variable "public_key_path" {
     description = <<DESCRIPTION
@@ -23,27 +43,26 @@ variable "public_key_path" {
 
 Example: ~/.ssh/my_key.pub
 DESCRIPTION
-    default = "~/.ssh/my_key.pub"
 }
 
 variable "key_name" {
   description = "Desired name of AWS key pair"
-  default = "my_key"
 }
 
 variable "aws_region" {
-  default = "us-east-1"
+  description = "example: us-east-1"
 }
 
-variable "s3_endpoint" {
-  default = "com.amazonaws.us-east-1.s3"
-}
 
 variable "domain_name" {
-  default = "linuxsuperhero"
+  description = "Only enter the domain portion. Do not enter the TLD. example: linuxsuperhero"
 }
 
 variable "delegation_set" {
-  default = "N3GTFFZI1MK1ON"
+    description = <<DESCRIPTION
+ This is obtained from
+ aws route53 create-reusable-delegation-set --caller-reference 1234
+ It will be a 14 digit alphanumeric string.
+DESCRIPTION
 }
 
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-region = "us-east-1"
-profile = "linuxacademy"
+region = "${var.aws_region}"
+profile = "${var.aws_profile}"
 }
+
 resource "aws_iam_instance_profile" "s3_access" {
     name = "s3_access"
     roles = ["${aws_iam_role.s3_access.name}"]
@@ -43,7 +44,7 @@ resource "aws_iam_role" "s3_access" {
 EOF
 }
 
-
+# Create VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "10.1.0.0/16"
 }
@@ -52,13 +53,6 @@ resource "aws_vpc" "vpc" {
 resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = "${aws_vpc.vpc.id}"
 }
-
-# Give the VPC internet access on its main route table
-# resource "aws_route" "internet_access" {
-#  route_table_id= "${aws_vpc.vpc.id}"
-#   destination_cidr_block = "0.0.0.0/0"
-#   gateway_id= "${aws_internet_gateway.internet-gateway.id}"
-#}
 
 resource "aws_route_table" "public" {
     vpc_id = "${aws_vpc.vpc.id}"
@@ -87,7 +81,7 @@ resource "aws_subnet" "public" {
   availability_zone = "us-east-1d"
 
   tags {
-    Name = "Public"
+    Name = "public"
   }
 }
 
@@ -202,14 +196,14 @@ resource "aws_db_subnet_group" "rds_subnetgroup" {
   }
 }
 
-#Public security group to access SSH,HTTP
-resource "aws_security_group" "Public" {
+#Public security group to access SSH from only your IP and HTTP
+resource "aws_security_group" "public" {
   name= "sg_public"
   description = "Used for public instances"
   vpc_id      = "${aws_vpc.vpc.id}"
 
 
-# SSH access from your local ip curl canhazip.com for an easy way to find it. 
+# SSH access from your local ip $(curl canhazip.com) for an easy way to find it. 
 ingress {
 from_port    = 22
 to_port      = 22
@@ -265,39 +259,39 @@ resource "aws_security_group" "RDS" {
   description = "Used for DB instances"
   vpc_id      = "${aws_vpc.vpc.id}"
 
-# SQL access from Public security group
+# SQL access from public security group
 ingress {
 from_port    = 3306
 to_port      = 3306
 protocol     = "tcp"
-security_groups  = ["${aws_security_group.Public.id}", "${aws_security_group.private.id}"]
+security_groups  = ["${aws_security_group.public.id}", "${aws_security_group.private.id}"]
  }
 }
 
-resource "aws_db_instance" "dmorgantest" {
+resource "aws_db_instance" "db" {
     allocated_storage 		= 10
     engine			= "mysql"
     engine_version		= "5.6.27"
-    instance_class		= "db.t1.micro"
-    name			= "dmorgandb"
+    instance_class		= "${var.db_instance_class}"
+    name			= "${var.dbname}"
     username			= "${var.dbuser}"
     password			= "${var.dbpass}"
     db_subnet_group_name        = "rds_subnetgroup"                                                                                         vpc_security_group_ids      = ["${aws_security_group.RDS.id}"]
-   # parameter_group_name	= "default.mysql5.6"
 
-##Add local-exec to run ansible wordpress_database setup here
 }
 
-
-# Create S3 buckets code and media
+# Create AWS key pair from local key
 
 resource "aws_key_pair" "auth" {
     key_name    = "${var.key_name}"
     public_key  = "${file(var.public_key_path)}"
 }
 
-resource "aws_s3_bucket" "la_code" {  
-    bucket = "la_code1110"  
+
+# Create S3 buckets code and media
+
+resource "aws_s3_bucket" "code" {  
+    bucket = "${var.domain_name}_code1115"  
     acl = "private"  
     force_destroy = true
 tags {  
@@ -306,8 +300,8 @@ Name = "code bucket"
 }  
 }  
   
-resource "aws_s3_bucket" "la_media" {  
-    bucket = "la_media1110"  
+resource "aws_s3_bucket" "media" {  
+    bucket = "${var.domain_name}_media1115"  
     acl = "private"
     force_destroy = true  
   
@@ -319,15 +313,15 @@ Name = "media bucket"
 
 # Create the master dev server
 
-resource "aws_instance" "golden" {
-  instance_type = "t2.micro"
-  ami = "ami-b73b63a0"
+resource "aws_instance" "dev" {
+  instance_type = "${var.dev_instance_type}"
+  ami = "${var.dev_ami}"
 
   key_name = "${aws_key_pair.auth.id}"
-  vpc_security_group_ids = ["${aws_security_group.Public.id}"]
+  vpc_security_group_ids = ["${aws_security_group.public.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
   provisioner "local-exec" {
-      command = "echo [dev] > ~/la_terraform/aws_hosts && echo ${aws_instance.golden.public_ip} >> ~/la_terraform/aws_hosts"
+      command = "echo [dev] > ~/la_terraform/aws_hosts && echo ${aws_instance.dev.public_ip} >> ~/la_terraform/aws_hosts"
   }
 
 # We're going to launch the dev server into a public subnet for setup.
@@ -344,9 +338,9 @@ resource "aws_instance" "golden" {
 # Create the load balancer
 
 resource "aws_elb" "prod" {
-  name = "dmorgansite-prod-elb"    
+  name = "${var.domain_name}-prod-elb"    
   subnets = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}"]
-  security_groups = ["${aws_security_group.Public.id}"]
+  security_groups = ["${aws_security_group.public.id}"]
   listener { 
     instance_port = 80
     instance_protocol = "http"
@@ -355,11 +349,11 @@ resource "aws_elb" "prod" {
   }
 
   health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 3
+    healthy_threshold = "${var.elb_healthy_threshold}"
+    unhealthy_threshold = "${var.elb_unhealthy_threshold}"
+    timeout = "${var.elb_timeout}"
     target = "HTTP:80/"
-    interval = 30
+    interval = "${var.elb_interval}"
   }
 
   cross_zone_load_balancing = true
@@ -368,19 +362,21 @@ resource "aws_elb" "prod" {
   connection_draining_timeout = 400
   
   tags {
-    Name = "dmorgansite-prod-elb"
+    Name = "${var.domain_name}-prod-elb"
   }
 }
 
 resource "aws_ami_from_instance" "golden" {
     name = "golden-image"
-    source_instance_id = "${aws_instance.golden.id}"
+    source_instance_id = "${aws_instance.dev.id}"
 }
 
-resource "aws_launch_configuration" "dmorgantest_lc" {
-    name_prefix   = "dmorgantest_lc-"
+# Create Launch configuration from dev ami
+
+resource "aws_launch_configuration" "lc" {
+    name_prefix   = "lc-"
     image_id      = "${aws_ami_from_instance.golden.id}"
-    instance_type = "t2.micro"
+    instance_type = "${var.lc_instance_type}"
     security_groups = ["${aws_security_group.private.id}"]
     iam_instance_profile = "${aws_iam_instance_profile.s3_access.id}"
     key_name = "${aws_key_pair.auth.id}"
@@ -390,22 +386,28 @@ resource "aws_launch_configuration" "dmorgantest_lc" {
     }
 }
 
-resource "aws_autoscaling_group" "dmorgantest_asg" {
-    availability_zones = ["us-east-1a", "us-east-1c"]
-    name = "dmorgantest_asg" 
-    max_size = 2
-    min_size = 1
-    health_check_grace_period = 300
-    health_check_type = "EC2"
-    desired_capacity = 2
+
+resource "random_id" "asg" {
+ byte_length = 8
+}
+
+
+resource "aws_autoscaling_group" "asg" {
+    availability_zones = ["${var.aws_region}a", "${var.aws_region}c"]
+    name = "asg-{random_id.asg.decimal}" 
+    max_size = "${var.asg_max}"
+    min_size = "${var.asg_min}"
+    health_check_grace_period = "${var.asg_grace}"
+    health_check_type = "${var.asg_hct}"
+    desired_capacity = "${var.asg_cap}"
     force_delete = true
     load_balancers = ["${aws_elb.prod.id}"]
     vpc_zone_identifier = ["${aws_subnet.private1.id}", "${aws_subnet.private2.id}"]
-    launch_configuration = "${aws_launch_configuration.dmorgantest_lc.name}"
+    launch_configuration = "${aws_launch_configuration.lc.name}"
     
     tag {
       key = "Name"
-      value = "dmorgantestweb"
+      value = "asg"
       propagate_at_launch = true
     }
 
@@ -438,7 +440,7 @@ resource "aws_route53_record" "dev" {
   name = "dev.${var.domain_name}.com"
   type = "A"
   ttl = "300"
-  records = ["${aws_instance.golden.public_ip}"]
+  records = ["${aws_instance.dev.public_ip}"]
 }
 
 resource "aws_route53_record" "db" {
@@ -446,7 +448,7 @@ resource "aws_route53_record" "db" {
   name = "db.${var.domain_name}.com"
   type = "CNAME"
   ttl = "300"
-  records = ["${aws_db_instance.dmorgantest.address}"]
+  records = ["${aws_db_instance.db.address}"]
 }
 
 
